3ced1f586de918457149accc2188da4a1fb1f491
Женя Ветров <pewdeiniro@gmail.com>
2021-04-28 18:16:12 +0300

    Added ImageButtons

--- core/src/com/evo/game/EvolutionGame.java
+++ core/src/com/evo/game/EvolutionGame.java
@@ -5,9 +5,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 
 public class EvolutionGame extends ApplicationAdapter {
@@ -15,7 +19,7 @@ public class EvolutionGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Stage game, menu, pause, mine1, mine2, mine3;
 	String scene = "menu";
-	//ImageButton startGameButton, continueGameButton, exitGameButton, continuePauseButton, exitPauseButton;
+	ImageButton startGameButton, continueGameButton, exitGameButton, continuePauseButton, exitPauseButton;
 	
 	@Override
 	public void create () {
@@ -26,7 +30,58 @@ public class EvolutionGame extends ApplicationAdapter {
 		mine1 = new Stage();
 		mine2 = new Stage();
 		mine3 = new Stage();
+
+		startGameButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture("startGameButton.png"))));
+		continueGameButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture("continueGameButton.png"))));
+		exitGameButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture("exitGameButton.png"))));
+		continuePauseButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture("continueGameButton.png"))));
+		exitPauseButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture("exitGameButton.png"))));
+
+		exitGameButton.addListener(new ClickListener(){
+
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				saveGame();
+				System.exit(0);
+			}
+		});
+
+		exitGameButton.addListener(new ClickListener(){
+
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				newGame();
+			}
+		});
+
+		continueGameButton.addListener(new ClickListener(){
+
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				continueGame();
+			}
+		});
+
+		menu.addActor(startGameButton);
+		menu.addActor(continueGameButton);
+		menu.addActor(exitGameButton);
+
+		pause.addActor(continuePauseButton);
+		pause.addActor(exitPauseButton);
+
 		Gdx.input.setInputProcessor(menu);
+	}
+
+	public void saveGame(){
+
+	}
+
+	public void newGame(){
+
+	}
+
+	public void continueGame(){
+
 	}
 
 	@Override
@@ -64,5 +119,8 @@ public class EvolutionGame extends ApplicationAdapter {
 		game.dispose();
 		pause.dispose();
 		menu.dispose();
+		mine1.dispose();
+		mine2.dispose();
+		mine3.dispose();
 	}
 }
